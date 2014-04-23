81a5af5e504feda148cc801542d9157ae2e719df
Arne Schwabe <arne@rfc2549.org>
2014-04-23 12:30:56 +0200

    Add changes to be able to compile wight the Android NDK
    Change-Id: I4c84a4463d26a668ab36338e80686ae1220cf13a

--- Ssl.mk
+++ Ssl.mk
@@ -1,8 +1,12 @@
 #######################################
 # target static library
 include $(CLEAR_VARS)
+<<<<<<< HEAD
 LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
 LOCAL_C_INCLUDES := $(log_c_includes)
+=======
+include $(LOCAL_PATH)/ndk-build-clear.mk
+>>>>>>> Add changes to be able to compile wight the Android NDK
 
 # The static library should be used in only unbundled apps
 # and we don't have clang in unbundled build yet.
@@ -16,13 +20,18 @@ LOCAL_MODULE := libssl_static
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
 include $(LOCAL_PATH)/Ssl-config-target.mk
 include $(LOCAL_PATH)/android-config.mk
+include $(LOCAL_PATH)/ndk-build.mk
 include $(BUILD_STATIC_LIBRARY)
 
 #######################################
 # target shared library
 include $(CLEAR_VARS)
+<<<<<<< HEAD
 LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
 LOCAL_C_INCLUDES := $(log_c_includes)
+=======
+include $(LOCAL_PATH)/ndk-build-clear.mk
+>>>>>>> Add changes to be able to compile wight the Android NDK
 
 # If we're building an unbundled build, don't try to use clang since it's not
 # in the NDK yet. This can be removed when a clang version that is fast enough
@@ -39,37 +48,39 @@ LOCAL_MODULE := libssl
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
 include $(LOCAL_PATH)/Ssl-config-target.mk
 include $(LOCAL_PATH)/android-config.mk
+include $(LOCAL_PATH)/ndk-build.mk
 include $(BUILD_SHARED_LIBRARY)
 
-#######################################
-# host shared library
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
-LOCAL_C_INCLUDES := $(log_c_includes)
 
-LOCAL_SHARED_LIBRARIES += libcrypto-host
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := libssl-host
-LOCAL_MULTILIB := both
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
-include $(LOCAL_PATH)/Ssl-config-host.mk
-include $(LOCAL_PATH)/android-config.mk
-include $(BUILD_HOST_SHARED_LIBRARY)
+# #######################################
+# # host shared library
+# include $(CLEAR_VARS)
+# LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
+# LOCAL_C_INCLUDES := $(log_c_includes)
 
-#######################################
-# ssltest
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
-LOCAL_C_INCLUDES := $(log_c_includes)
+# LOCAL_SHARED_LIBRARIES += libcrypto-host
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_MODULE := libssl-host
+# LOCAL_MULTILIB := both
+# LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
+# include $(LOCAL_PATH)/Ssl-config-host.mk
+# include $(LOCAL_PATH)/android-config.mk
+# include $(BUILD_HOST_SHARED_LIBRARY)
 
-LOCAL_SRC_FILES := ssl/ssltest.c
-LOCAL_SHARED_LIBRARIES := libssl libcrypto
-LOCAL_MODULE := ssltest
-LOCAL_MULTILIB := both
-LOCAL_MODULE_STEM_32 := ssltest
-LOCAL_MODULE_STEM_64 := ssltest64
-LOCAL_MODULE_TAGS := optional
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
-include $(LOCAL_PATH)/Ssl-config-host.mk
-include $(LOCAL_PATH)/android-config.mk
-include $(BUILD_EXECUTABLE)
+# #######################################
+# # ssltest
+# include $(CLEAR_VARS)
+# LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
+# LOCAL_C_INCLUDES := $(log_c_includes)
+
+# LOCAL_SRC_FILES := ssl/ssltest.c
+# LOCAL_SHARED_LIBRARIES := libssl libcrypto
+# LOCAL_MODULE := ssltest
+# LOCAL_MULTILIB := both
+# LOCAL_MODULE_STEM_32 := ssltest
+# LOCAL_MODULE_STEM_64 := ssltest64
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
+# include $(LOCAL_PATH)/Ssl-config-host.mk
+# include $(LOCAL_PATH)/android-config.mk
+# include $(BUILD_EXECUTABLE)
