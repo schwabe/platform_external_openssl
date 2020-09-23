7220616195ab8a8e098107bf80c8c3382ab16c90
Arne Schwabe <arne@rfc2549.org>
2020-09-23 09:14:56 +0200

    Import OpenSSL 1.1.1h

--- crypto/buildinf.h
+++ crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-x86_64"
-#define DATE "built on: Tue Apr 21 14:46:43 2020 UTC"
+#define DATE "built on: Wed Sep 23 07:10:30 2020 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
