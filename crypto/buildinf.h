3d97ad4d0d003fcb07b538885ab4d0524c67489b
Arne Schwabe <arne@openvpn.net>
2022-03-15 19:57:26 +0100

    Import OpenSSL 1.1.1n

--- crypto/buildinf.h
+++ crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-x86_64"
-#define DATE "built on: Tue Aug 24 17:28:26 2021 UTC"
+#define DATE "built on: Tue Mar 15 18:56:49 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
